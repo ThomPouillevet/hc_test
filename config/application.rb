7fc6136a27671b5a4f510feab420bcbd03cfdc86
Thomas Pouillevet <thomas.pouillevet@gmail.com>
2019-03-13 15:50:41 +0100

    adds factory girl gem

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,13 @@ Bundler.require(*Rails.groups)
 
 module HcTest
   class Application < Rails::Application
-    config.generators do |generate|
-          generate.assets false
-          generate.helper false
-          generate.test_framework  :test_unit, fixture: false
-        end
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        request_specs: false
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
