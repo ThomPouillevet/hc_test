f0551cfa23f7dcbdf2709ca66c0efdf5bcd0d02f
Thomas Pouillevet <thomas.pouillevet@gmail.com>
2019-03-16 14:52:42 +0100

    adds position to request to reconfirm interest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_105716) do
+ActiveRecord::Schema.define(version: 2019_03_16_135058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_03_14_105716) do
     t.datetime "updated_at", null: false
     t.boolean "email_confirmed", default: false
     t.string "confirm_token"
+    t.integer "position"
   end
 
 end
